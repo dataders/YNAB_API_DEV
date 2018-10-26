76e1c90e8a7259c6a1208948b4fe3652c6af7817
Anders Swanson <anders.swanson@avanade.com>
2018-10-26 14:29:08 -0700

    get budget categories

--- connect.py
+++ connect.py
@@ -1,4 +1,4 @@
-
+import os
 import requests
 import json
 
@@ -6,7 +6,23 @@ f = open('token.txt', 'r')
 
 token = f.read()
 
+auth = " ".join(["Bearer", token])
+auth
+
 f.close()
 
+url = 'https://api.youneedabudget.com\\v1\\budgets'
+header = {'Authorization':auth}
 
-headers = {'Authorization headers'}
+r = requests.get(url, headers = header)
+output = r.json()
+
+import json
+with open('output/data.json', 'w') as outfile:
+    json.dump(output, outfile)
+
+output = r.json()
+
+budget_id = output["data"]["budgets"][0]["id"]
+
+url_category = os.path.join(url, budget_id, 'categories')
